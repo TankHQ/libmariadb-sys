becbfe1e0818490b76d5ac1ca3a377f7a76cc792
barsdeveloper <barsdeveloper@gmail.com>
2025-11-14 00:46:41 +0200

    buildtime-bindgen

--- build.rs
+++ build.rs
@@ -14,12 +14,14 @@ fn main() {
     println!("cargo:rustc-link-lib=static=mariadbclient");
     println!("cargo:rerun-if-changed=source/include");
 
-    let bindings = bindgen::Builder::default()
-        .header(dst.join("include/mariadb/mysql.h").to_str().unwrap())
-        .generate()
-        .expect("Unable to generate bindings");
-
-    bindings
-        .write_to_file("bindings/bindings.rs")
-        .expect("Couldn't write bindings!");
+    #[cfg(feature = "buildtime-bindgen")]
+    {
+        let bindings = bindgen::Builder::default()
+            .header(dst.join("include/mariadb/mysql.h").to_str().unwrap())
+            .generate()
+            .expect("Unable to generate bindings");
+        bindings
+            .write_to_file("bindings/bindings.rs")
+            .expect("Couldn't write bindings!");
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,12 @@
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+
 mod bindings {
+    #[cfg(feature = "buildtime-bindgen")]
+    include!(concat!(env!("OUT_DIR"), "/bindings/bindings.rs"));
+
+    #[cfg(not(feature = "buildtime-bindgen"))]
     include!(concat!(env!("CARGO_MANIFEST_DIR"), "/bindings/bindings.rs"));
 }
 
